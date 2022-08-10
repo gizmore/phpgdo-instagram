57db011ef20ff933b453e45be649c60bb0044835
gizmore <gizmore@wechall.net>
2022-08-10 20:36:04 +0200

    '7.0.1-r1418'

--- tpl/cell/igauthbutton.php
+++ tpl/cell/igauthbutton.php
@@ -3,4 +3,4 @@ use GDO\UI\GDT_Link;
 use GDO\Instagram\GDT_IGAuthButton;
 $field instanceof GDT_IGAuthButton;
 $icon = sprintf('<img src="GDO/Instagram/img/Instagram_icon.png" title="%s" />', t('btn_continue_with_ig'));
-echo GDT_Link::make()->noLabel()->href($field->href)->rawIcon($icon)->render();
+echo GDT_Link::make()->labelNone()->href($field->href)->rawIcon($icon)->render();
